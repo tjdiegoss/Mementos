04b8dd4d3454b2d58c0ef2a9655f56d3fb0cf608
tjdiegoss <tjdiegoss@gmail.com>
2022-09-06 01:17:35 -0300

    Primeiro commit!

---
+++ Mementos/src/main/java/br/com/tjdiego/mementos/Mementos.java
@@ -0,0 +1,14 @@
+
+
+package br.com.tjdiego.mementos;
+
+/**
+ *
+ * @author Diego
+ */
+public class Mementos {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
